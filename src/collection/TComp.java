38e37f4e17b4772a82ef3e593b25b324acfa70a8
lganameatwork <13419866507@163.com>
2019-04-18 20:36:13 +0800

    add .gitignore

---
+++ src/collection/TComp.java
@@ -0,0 +1,12 @@
+package collection;
+
+import java.util.Comparator;
+
+public class TComp implements Comparator<String> {
+    public int compare(String aStr, String bStr){
+        int i,j,k;
+
+        //Find index of beginning of last name
+        i = aStr.lastIndexOf()
+    }
+}
